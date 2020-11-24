49e9aaa7d3fee20ce3ace81e3391e2e5054d77df
arng0123 <60373058+arng0123@users.noreply.github.com>
2020-11-24 10:51:08 -0500

    Update AlgoExpert_Easy_RunLengthEncoding.js

--- AlgoExpert_Easy_RunLengthEncoding.js
+++ AlgoExpert_Easy_RunLengthEncoding.js
@@ -1,3 +1,4 @@
+// First Attempt
 function runLengthEncoding(string, newStr = '') {
 	let curr = string[0]
 	let num = 0
@@ -17,3 +18,29 @@ function runLengthEncoding(string, newStr = '') {
   
 	return runLengthEncoding(string.slice(num), newStr)
 }
+
+
+//Refactored
+function runLengthEncoding(string) {
+	let str = []
+	let runLength = 1
+	
+	for(let i = 1; i<string.length; i++){
+		let prevChar = string[i-1]
+		let currentChar = string[i]
+		
+		if(runLength === 9 || prevChar !== currentChar){
+			str.push(runLength.toString())
+			str.push(prevChar)
+			runLength = 0
+		}
+		
+		runLength++
+	}
+	
+	str.push(runLength.toString())
+	str.push(string[string.length-1])
+	
+	return str.join('')
+	
+	}
